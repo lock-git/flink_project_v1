14a2f72b339b274e458569489ac15696ccd10a36
xiashiyong <xiashiyong@jiangduoduo.com>
2020-12-18 21:09:35 +0800

    小项目开始

---
+++ flink_2020/HotItemsAnalysis/src/main/scala/com/lock/hotitems/CaseModel.scala
@@ -0,0 +1,13 @@
+package com.lock.hotitems
+
+/**
+  * 定义输入数据的样例类
+  *
+  * author  Lock.xia
+  * Date 2020-12-18
+  */
+object CaseModel {
+
+}
+
+case class UserBehavior(userId: Long, itemId: Long, behavior: String, timestamp: Long)
---
+++ flink_2020/HotItemsAnalysis/src/main/scala/com/lock/hotitems/HotItems.scala
@@ -0,0 +1,16 @@
+package com.lock.hotitems
+
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+
+/**
+  * author  Lock.xia
+  * Date 2020-12-18
+  */
+object HotItems {
+
+  def main(args: Array[String]): Unit = {
+
+    // 创建一个流式处理执行环境
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+  }
+}
