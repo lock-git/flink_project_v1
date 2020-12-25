97d0b182f5e6be7175dfb942b2094461db376ac2
xiashiyong <xiashiyong@jiangduoduo.com>
2020-12-25 16:21:54 +0800

    对延迟数据进行3种方式的处理和理解

--- flink_2020/NetworkFlowAnalysis/src/main/scala/com/lock/network/dataanalysis/NetworkFlowPageTop_P.scala
+++ flink_2020/NetworkFlowAnalysis/src/main/scala/com/lock/network/dataanalysis/NetworkFlowPageTop_P.scala
@@ -10,6 +10,9 @@ import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironm
 import org.apache.flink.streaming.api.windowing.time.Time
 
 /**
+  *
+  * 对于延迟数据，有三种方式解决【watermark allowed-lateness sideOutput】
+  *
   * author  Lock.xia
   * Date 2020-12-24
   */
@@ -22,11 +25,15 @@ object NetworkFlowPageTop_P {
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
 
 
-    val inputStream: DataStream[String] = env.readTextFile("E:\\Code\\bigdata\\flink_project_git\\flink_2020\\NetworkFlowAnalysis\\src\\main\\resources\\apache.log")
+    //val inputStream: DataStream[String] = env.readTextFile("E:\\Code\\bigdata\\flink_project_git\\flink_2020\\NetworkFlowAnalysis\\src\\main\\resources\\apache.log")
+
+    val inputStream: DataStream[String] = env.socketTextStream("localhost",7777) // [ nc -l -p 7777 ]
+
+    // 侧输出流的标签
     val lateDataTag = new OutputTag[ApacheLogEvent]("late_data")
 
     // 83.149.9.216 - - 17/05/2015:10:05:56 +0000 GET /favicon.ico
-    val aggStream: DataStream[PageViewCount] = inputStream
+    val dataStream: DataStream[ApacheLogEvent] = inputStream
       .map((k: String) => {
         val dataArr: Array[String] = k.split(" ")
         val time: Long = new SimpleDateFormat("dd/MM/yyyy:HH:mm:ss").parse(dataArr(3)).getTime
@@ -37,20 +44,24 @@ object NetworkFlowPageTop_P {
           element.eventTime
         }
       })
+
+    val aggStream: DataStream[PageViewCount] = dataStream
       .keyBy((_: ApacheLogEvent).url)
       .timeWindow(Time.minutes(10), Time.seconds(5))
-      .allowedLateness(Time.minutes(1)) // 等待迟到数据 1min ，将之前窗口的数据迭代
+      .allowedLateness(Time.minutes(1)) //[ 定义延迟时间 ] 等待迟到数据 1 min ，将之前窗口的数据和延迟的数据一起聚合，再更新迭代数据，等待新的watermark触发输出数据，最后等延迟时间到了再关闭窗口，清空窗口数据
       .sideOutputLateData(lateDataTag) // 侧输出流
       .aggregate(new PrePageCount(), new PageWindowFunction())
 
+    // 获取侧输出流的数据 ==》 可以跟之前的流做一个合流操作，得到最终的数据
+    val lateStream: DataStream[ApacheLogEvent] = aggStream.getSideOutput(lateDataTag)
 
     val resultStream: DataStream[String] = aggStream
       .keyBy((_: PageViewCount).windowEnd)
       .process(new PageCountProcessFunction(5))
 
-    // 迟到数据 单独操作，最后再合流
-    val lateStream: DataStream[ApacheLogEvent] = aggStream.getSideOutput(lateDataTag)
-
+    dataStream.print("input")
+    aggStream.print("agg")
+    lateStream.print("late")
     resultStream.print("topResult")
 
     env.execute("page top n")
--- flink_2020/NetworkFlowAnalysis/src/main/scala/com/lock/network/function/PageCountProcessFunction.scala
+++ flink_2020/NetworkFlowAnalysis/src/main/scala/com/lock/network/function/PageCountProcessFunction.scala
@@ -1,10 +1,9 @@
 package com.lock.network.function
 
 import java.sql.Timestamp
-import java.util
 
 import com.lock.network.entry.PageViewCount
-import org.apache.flink.api.common.state.{ListState, ListStateDescriptor}
+import org.apache.flink.api.common.state.{MapState, MapStateDescriptor}
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction
 import org.apache.flink.util.Collector
 
@@ -16,37 +15,44 @@ import scala.collection.mutable.ListBuffer
   */
 class PageCountProcessFunction(topN: Int) extends KeyedProcessFunction[Long, PageViewCount, String] {
 
-  lazy val pageListState: ListState[PageViewCount] = getRuntimeContext.getListState[PageViewCount](new ListStateDescriptor[PageViewCount]("page_list_state", classOf[PageViewCount]))
+
+  // 定义一个 mapState ，通过key更新数据
+  lazy val pageMapState: MapState[String, Long] = getRuntimeContext.getMapState[String, Long](new MapStateDescriptor[String, Long]("page_map_state", classOf[String], classOf[Long]))
 
 
   override def processElement(value: PageViewCount, ctx: KeyedProcessFunction[Long, PageViewCount, String]#Context, out: Collector[String]): Unit = {
 
-    pageListState.add(value)
+    pageMapState.put(value.url, value.count)
     ctx.timerService().registerEventTimeTimer(value.windowEnd + 1)
+    ctx.timerService().registerEventTimeTimer(value.windowEnd + 60 * 1000)
 
   }
 
   override def onTimer(timestamp: Long, ctx: KeyedProcessFunction[Long, PageViewCount, String]#OnTimerContext, out: Collector[String]): Unit = {
 
-    val stateData: util.Iterator[PageViewCount] = pageListState.get().iterator()
-
-    val page_lsit: ListBuffer[PageViewCount] = new ListBuffer[PageViewCount]()
-
-    import scala.collection.JavaConversions._
-    for (p <- stateData) {
-      page_lsit += p
+    // 时间到了延迟时间，清空状态
+    if (timestamp == (ctx.getCurrentKey + 60 * 1000)) {
+      pageMapState.clear()
+      return
     }
 
-    val sortedList: List[PageViewCount] = page_lsit.toList.sortBy((_: PageViewCount).count)(Ordering.Long.reverse).take(topN)
+    val page_lsit: ListBuffer[(String, Long)] = new ListBuffer[(String, Long)]()
+
+    import scala.collection.JavaConversions._
+    for (p <- pageMapState.entries()) {
+      page_lsit += ((p.getKey, p.getValue))
+    }
+
+    val sortedList: List[(String, Long)] = page_lsit.toList.sortBy((_: (String, Long))._2)(Ordering.Long.reverse).take(topN)
 
     val resultStr: StringBuilder = new StringBuilder()
     resultStr.append(s"time: ${new Timestamp(timestamp - 1)} \n")
     for (i <- sortedList.indices) {
-      resultStr.append(s"TOP ${i + 1} : url===>${sortedList(i).url},counts===>${sortedList(i).count}\n")
+      resultStr.append(s"TOP ${i + 1} : url===>${sortedList(i)._1},counts===>${sortedList(i)._2}\n")
     }
     resultStr.append("==================================================================\n\n\n")
 
     out.collect(resultStr.toString())
-    pageListState.clear()
+
   }
 }
